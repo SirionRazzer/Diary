748d3ff3b5cc90ce5ccbb14b8cbbb5eeabbfc73d
SirionRazzer <soukaltom@gmail.com>
2019-03-19 16:28:11 +0100

    Added classes + their layout files

---
+++ app/src/main/java/com/sirionrazzer/diary/TrackItemCreatorActivity.kt
@@ -0,0 +1,12 @@
+package com.sirionrazzer.diary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TrackItemCreatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trackitem_creator)
+    }
+}
