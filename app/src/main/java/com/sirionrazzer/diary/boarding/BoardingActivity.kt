5c56cc6850dacb6f2417b17c6b78f711fcbfb35b
Tomáš Soukal <soukaltom@gmail.com>
2019-04-02 11:44:32 +0200

    Added toolbar

--- app/src/main/java/com/sirionrazzer/diary/boarding/BoardingActivity.kt
+++ app/src/main/java/com/sirionrazzer/diary/boarding/BoardingActivity.kt
@@ -3,9 +3,11 @@ package com.sirionrazzer.diary.boarding
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.sirionrazzer.diary.R
 import com.sirionrazzer.diary.history.HistoryActivity
 import kotlinx.android.synthetic.main.activity_boarding.*
+import kotlinx.android.synthetic.main.toolbar.*
 import org.jetbrains.anko.startActivity
 
 class BoardingActivity : AppCompatActivity() {
@@ -15,9 +17,20 @@ class BoardingActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_boarding)
 
+        setSupportActionBar(toolbar)
+        toolbar.title = "Diary"
+        toolbar.visibility = View.GONE
+
         signIn.setOnClickListener{
+            // TODO: store values of name and password
+            startActivity<HistoryActivity>()
+            finish()
+        }
+
+        skipSignIn.setOnClickListener{
             startActivity<HistoryActivity>()
             finish()
         }
     }
+
 }
--- app/src/main/java/com/sirionrazzer/diary/history/HistoryActivity.kt
+++ app/src/main/java/com/sirionrazzer/diary/history/HistoryActivity.kt
@@ -3,11 +3,15 @@ package com.sirionrazzer.diary.history
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.sirionrazzer.diary.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class HistoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        setSupportActionBar(toolbar)
+        toolbar.title = ""
     }
 }
