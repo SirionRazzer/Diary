996abe3bd1910bba7392dc365bb6b2f810062257
Ondrej Zeman <ozeman77@gmail.com>
2019-04-30 16:32:46 +0200

    fixed password error message, introduced string variables and added misc.xml to gitignore

--- app/src/main/java/com/sirionrazzer/diary/boarding/BoardingActivity.kt
+++ app/src/main/java/com/sirionrazzer/diary/boarding/BoardingActivity.kt
@@ -18,7 +18,6 @@ import org.jetbrains.anko.startActivity
 
 class BoardingActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
@@ -28,13 +27,15 @@ class BoardingActivity : AppCompatActivity() {
 
         login()
         setSupportActionBar(toolbar)
-        toolbar.title = "Diary"
+        toolbar.title = getString(R.string.app_name)
         toolbar.visibility = View.GONE
 
         signIn.setOnClickListener {
             val email = etEmail.text.toString()
             val pw = etPassword.text.toString()
-            if (isValidEmail(email) && !pw.isBlank()) {
+            if (pw.isBlank() || pw.length < 6) {
+                Toast.makeText(this, getString(R.string.short_password), Toast.LENGTH_SHORT).show()
+            } else if (isValidEmail(email)) {
                 auth.signInWithEmailAndPassword(email, pw)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
@@ -67,18 +68,21 @@ class BoardingActivity : AppCompatActivity() {
 
     private fun showDialog(auth: FirebaseAuth, email: String, pw: String) {
         val builder = AlertDialog.Builder(this)
-        builder.setMessage("Would you like to create a new account? If you are not new, then your credentials were incorrect.")
-            .setTitle("Hey there")
+        builder.setMessage(getString(R.string.new_account_or_incorrect_input))
+            .setTitle(getString(R.string.boarding_greetings))
             .setCancelable(false)
-            .setPositiveButton("Yes") { dialog, id ->
+            .setPositiveButton(getString(R.string.yes)) { dialog, id ->
                 auth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener {
-                    Toast.makeText(this, "Thank you for signing up", Toast.LENGTH_LONG).show()
-                    startActivity<HistoryActivity>()
-                    finish()
-
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, getString(R.string.success_signup_message), Toast.LENGTH_LONG).show()
+                        startActivity<HistoryActivity>()
+                        finish()
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
                 }
             }
-            .setNegativeButton("No") { dialog, id -> dialog.cancel() }
+            .setNegativeButton(getString(R.string.no)) { dialog, id -> dialog.cancel() }
         val alert = builder.create()
         alert.show()
         builder.show()
@@ -93,7 +97,7 @@ class BoardingActivity : AppCompatActivity() {
         if (user != null) {
             startActivity(Intent(this, HistoryActivity::class.java))
             finish()
-            Toast.makeText(this, "You've been successfully logged in!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.success_login_message), Toast.LENGTH_SHORT).show()
         }
     }
 
